f645b6a0a41917e2fde5fdf5366a0043149c2aea
laur <igor.laur2@gmail.com>
2018-12-05 17:37:55 -0200

    Initial Commit

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15965,6 +15965,137 @@ const Util = (($) => {
 
 export default Util
 
+/* eslint-env node */
+/* eslint no-process-env: 0 */
+
+module.exports = (config) => {
+  const jqueryFile = process.env.USE_OLD_JQUERY ? 'https://code.jquery.com/jquery-1.9.1.min.js' : 'site/docs/4.1/assets/js/vendor/jquery-slim.min.js'
+
+  config.set({
+    basePath: '../..',
+    frameworks: ['qunit', 'sinon', 'detectBrowsers'],
+    plugins: [
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'karma-qunit',
+      'karma-sinon',
+      'karma-detect-browsers'
+    ],
+    // list of files / patterns to load in the browser
+    files: [
+      jqueryFile,
+      'site/docs/4.1/assets/js/vendor/popper.min.js',
+      'dist/js/bootstrap.js',
+      'js/tests/unit/*.js'
+    ],
+    reporters: ['dots'],
+    port: 9876,
+    colors: true,
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_ERROR || config.LOG_WARN,
+    autoWatch: false,
+    customLaunchers: {
+      FirefoxHeadless: {
+        base: 'Firefox',
+        flags: ['-headless']
+      }
+    },
+    singleRun: true,
+    concurrency: Infinity,
+    detectBrowsers: {
+      usePhantomJS: false,
+      postDetection(availableBrowser) {
+        if (typeof process.env.TRAVIS_JOB_ID !== 'undefined' || availableBrowser.includes('Chrome')) {
+          return ['ChromeHeadless']
+        }
+
+        if (availableBrowser.includes('Firefox')) {
+          return ['FirefoxHeadless']
+        }
+
+        throw new Error('Please install Firefox or Chrome')
+      }
+    }
+  })
+}
+
+/* eslint-env node */
+/* eslint no-process-env: 0 */
+const path = require('path')
+const jsCoveragePath = path.resolve(__dirname, '../coverage')
+
+module.exports = (config) => {
+  const jqueryFile = process.env.USE_OLD_JQUERY ? 'https://code.jquery.com/jquery-1.9.1.min.js' : 'site/docs/4.1/assets/js/vendor/jquery-slim.min.js'
+
+  config.set({
+    basePath: '../..',
+    frameworks: ['qunit', 'sinon', 'detectBrowsers'],
+    plugins: [
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'karma-qunit',
+      'karma-sinon',
+      'karma-detect-browsers',
+      'karma-coverage-istanbul-reporter'
+    ],
+    // list of files / patterns to load in the browser
+    files: [
+      jqueryFile,
+      'site/docs/4.1/assets/js/vendor/popper.min.js',
+      'js/coverage/dist/util.js',
+      'js/coverage/dist/tooltip.js',
+      'js/coverage/dist/!(util|index|tooltip).js', // include all of our js/dist files except util.js, index.js and tooltip.js
+      'js/tests/unit/*.js'
+    ],
+    reporters: ['dots', 'coverage-istanbul'],
+    port: 9876,
+    colors: true,
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_ERROR || config.LOG_WARN,
+    autoWatch: false,
+    customLaunchers: {
+      FirefoxHeadless: {
+        base: 'Firefox',
+        flags: ['-headless']
+      }
+    },
+    singleRun: true,
+    concurrency: Infinity,
+    detectBrowsers: {
+      usePhantomJS: false,
+      postDetection(availableBrowser) {
+        if (typeof process.env.TRAVIS_JOB_ID !== 'undefined' || availableBrowser.includes('Chrome')) {
+          return ['ChromeHeadless']
+        }
+
+        if (availableBrowser.includes('Firefox')) {
+          return ['FirefoxHeadless']
+        }
+
+        throw new Error('Please install Firefox or Chrome')
+      }
+    },
+    coverageIstanbulReporter: {
+      dir: jsCoveragePath,
+      reports: ['lcov', 'text-summary'],
+      thresholds: {
+        emitWarning: false,
+        global: {
+          statements: 90,
+          branches: 84,
+          functions: 87,
+          lines: 90
+        }
+      }
+    },
+    client: {
+      qunit: {
+        showUI: true
+      }
+    }
+  })
+}
+
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('jquery'), require('./util.js')) :
   typeof define === 'function' && define.amd ? define(['jquery', './util.js'], factory) :
@@ -20379,137 +20510,6 @@ export default Util
 
 })));
 //# sourceMappingURL=util.js.map
-
-/* eslint-env node */
-/* eslint no-process-env: 0 */
-
-module.exports = (config) => {
-  const jqueryFile = process.env.USE_OLD_JQUERY ? 'https://code.jquery.com/jquery-1.9.1.min.js' : 'site/docs/4.1/assets/js/vendor/jquery-slim.min.js'
-
-  config.set({
-    basePath: '../..',
-    frameworks: ['qunit', 'sinon', 'detectBrowsers'],
-    plugins: [
-      'karma-chrome-launcher',
-      'karma-firefox-launcher',
-      'karma-qunit',
-      'karma-sinon',
-      'karma-detect-browsers'
-    ],
-    // list of files / patterns to load in the browser
-    files: [
-      jqueryFile,
-      'site/docs/4.1/assets/js/vendor/popper.min.js',
-      'dist/js/bootstrap.js',
-      'js/tests/unit/*.js'
-    ],
-    reporters: ['dots'],
-    port: 9876,
-    colors: true,
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_ERROR || config.LOG_WARN,
-    autoWatch: false,
-    customLaunchers: {
-      FirefoxHeadless: {
-        base: 'Firefox',
-        flags: ['-headless']
-      }
-    },
-    singleRun: true,
-    concurrency: Infinity,
-    detectBrowsers: {
-      usePhantomJS: false,
-      postDetection(availableBrowser) {
-        if (typeof process.env.TRAVIS_JOB_ID !== 'undefined' || availableBrowser.includes('Chrome')) {
-          return ['ChromeHeadless']
-        }
-
-        if (availableBrowser.includes('Firefox')) {
-          return ['FirefoxHeadless']
-        }
-
-        throw new Error('Please install Firefox or Chrome')
-      }
-    }
-  })
-}
-
-/* eslint-env node */
-/* eslint no-process-env: 0 */
-const path = require('path')
-const jsCoveragePath = path.resolve(__dirname, '../coverage')
-
-module.exports = (config) => {
-  const jqueryFile = process.env.USE_OLD_JQUERY ? 'https://code.jquery.com/jquery-1.9.1.min.js' : 'site/docs/4.1/assets/js/vendor/jquery-slim.min.js'
-
-  config.set({
-    basePath: '../..',
-    frameworks: ['qunit', 'sinon', 'detectBrowsers'],
-    plugins: [
-      'karma-chrome-launcher',
-      'karma-firefox-launcher',
-      'karma-qunit',
-      'karma-sinon',
-      'karma-detect-browsers',
-      'karma-coverage-istanbul-reporter'
-    ],
-    // list of files / patterns to load in the browser
-    files: [
-      jqueryFile,
-      'site/docs/4.1/assets/js/vendor/popper.min.js',
-      'js/coverage/dist/util.js',
-      'js/coverage/dist/tooltip.js',
-      'js/coverage/dist/!(util|index|tooltip).js', // include all of our js/dist files except util.js, index.js and tooltip.js
-      'js/tests/unit/*.js'
-    ],
-    reporters: ['dots', 'coverage-istanbul'],
-    port: 9876,
-    colors: true,
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_ERROR || config.LOG_WARN,
-    autoWatch: false,
-    customLaunchers: {
-      FirefoxHeadless: {
-        base: 'Firefox',
-        flags: ['-headless']
-      }
-    },
-    singleRun: true,
-    concurrency: Infinity,
-    detectBrowsers: {
-      usePhantomJS: false,
-      postDetection(availableBrowser) {
-        if (typeof process.env.TRAVIS_JOB_ID !== 'undefined' || availableBrowser.includes('Chrome')) {
-          return ['ChromeHeadless']
-        }
-
-        if (availableBrowser.includes('Firefox')) {
-          return ['FirefoxHeadless']
-        }
-
-        throw new Error('Please install Firefox or Chrome')
-      }
-    },
-    coverageIstanbulReporter: {
-      dir: jsCoveragePath,
-      reports: ['lcov', 'text-summary'],
-      thresholds: {
-        emitWarning: false,
-        global: {
-          statements: 90,
-          branches: 84,
-          functions: 87,
-          lines: 90
-        }
-      }
-    },
-    client: {
-      qunit: {
-        showUI: true
-      }
-    }
-  })
-}
 
 $(function () {
   'use strict'
